f7be6c5ba4dc934fbc8806b3956070de5c6b7702
odfrank <oyedeledamilaref@yahoo.com>
2023-11-19 05:07:52 -0700

    Add Entity models for User_Expense and AppDbContext

---
+++ Trexpense.DB/Expense.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Trexpense.DB
+{
+    public class Expense
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Description  { get; set; }
+        public double Amount { get; set; }
+    }
+}
