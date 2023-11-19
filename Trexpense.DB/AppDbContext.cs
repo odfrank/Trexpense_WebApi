d740065e609af3b14406230cecbee876ba6741f1
odfrank <oyedeledamilaref@yahoo.com>
2023-11-19 05:26:51 -0700

    Adds first migrations to local database TrexpenseDb

--- Trexpense.DB/AppDbContext.cs
+++ Trexpense.DB/AppDbContext.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Trexpense.DB;
 
-namespace Trexpense.DB
+namespace Expenses.DB
 {
-    internal class Class1
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+
+        public DbSet<Expense> Expenses { get; set; }
+        //public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=TrexpenseDb;Trusted_Connection=True;");
+        }
     }
 }
